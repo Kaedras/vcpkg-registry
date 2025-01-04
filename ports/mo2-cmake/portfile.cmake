05b332b70c5230116977573ded9d64b8105413dc
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-01-04 15:44:02 +0100

    fixed mo2-cmake

--- ports/mo2-cmake/portfile.cmake
+++ ports/mo2-cmake/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Kaedras/cmake_common
     REF "v${VERSION}"
-    SHA512 0
+    SHA512 4d407de8a44391f66928b6d544d213efdecfb59b26ad13a6b90fce5e7ab3bf9bd0c18f64a72b8098b51b95b58aee88a36d6ef644ac316ad55ca140ca9c1bb77c
     HEAD_REF master
 )
 
