334a0f375bbc02708712422fb12af7080dc1f5fc
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-01-04 17:09:35 +0100

    fixed libbsarchpp

--- ports/libbsarchpp/portfile.cmake
+++ ports/libbsarchpp/portfile.cmake
@@ -1,17 +1,17 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Kaedras/libbsarchpp
-    REF 0
-    SHA512 0
+    REF 32bab7141ee2de72fafc71c5a651169bc2bf13a7
+    SHA512 56d0b767a881073d407a9fdc867cecadbf01f8d64b85cf5dbd2dde5924ab7d544a37f38e2e2d901c9944d376ca1ffb4134f35fd9d2cc69bb5ab7ddc5abceae20
     HEAD_REF master
 )
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-
-#vcpkg_cmake_config_fixup(PACKAGE_NAME "libbsarchpp" CONFIG_PATH "lib/cmake/libbsarchpp")
+vcpkg_cmake_config_fixup(PACKAGE_NAME "libbsarchpp" CONFIG_PATH "lib/cmake/libbsarchpp")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
 
-#file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
