2d96626ca3e733f02c66550ca0d21c85042f951f
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-01-15 22:46:25 +0100

    [libloot] Bump to 0.24.5

--- ports/libloot/portfile.cmake
+++ ports/libloot/portfile.cmake
@@ -1,12 +1,18 @@
-# download prebuilt library if target is windows
 if(VCPKG_TARGET_IS_WINDOWS)
-set(VCPKG_POLICY_DLLS_IN_STATIC_LIBRARY enabled)
+    set(VCPKG_POLICY_DLLS_IN_STATIC_LIBRARY enabled)
 
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/loot/libloot/releases/download/${VERSION}/libloot-${VERSION}-win64.7z"
-    FILENAME "libloot-${VERSION}-win64.7z"
-    SHA512 6559AB8A365A78B0A0C2F010276F1E04CECAA81BEB0006AB0565517B172728F5E3533DA4F0B9BB78972D886ED1605AF2418668216C6F34A05A6CAF94648E2DD4
-)
+    vcpkg_download_distfile(ARCHIVE
+        URLS "https://github.com/loot/libloot/releases/download/${VERSION}/libloot-${VERSION}-win64.7z"
+        FILENAME "libloot-${VERSION}-win64.7z"
+        SHA512 d32a7961aed70c91d980546664e464e262665eb97ea74ea26b4f5c9417e98384a7024d37d70271bd513621941c6999dfdbd15424821b011540ad818250fbadd9
+    )
+else()
+    vcpkg_download_distfile(ARCHIVE
+        URLS "https://github.com/loot/libloot/releases/download/${VERSION}/libloot-${VERSION}-Linux.tar.xz"
+        FILENAME "libloot-${VERSION}-Linux.tar.xz"
+        SHA512 2c54892185a4e7499cb6951d9446f1e33a46f6bd339d76a06bc09dd73a36bcc6c2438fb533ddac42d3be8dc520b4f4ade8fe1ade0abebfce65cf87853edee3d9
+    )
+endif()
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -20,41 +26,17 @@ vcpkg_download_distfile(LICENSE
 )
 
 file(INSTALL ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR})
-file(INSTALL ${SOURCE_PATH}/bin DESTINATION ${CURRENT_PACKAGES_DIR})
+if(VCPKG_TARGET_IS_WINDOWS)
+    file(INSTALL ${SOURCE_PATH}/bin DESTINATION ${CURRENT_PACKAGES_DIR})
+endif()
 file(INSTALL ${SOURCE_PATH}/lib DESTINATION ${CURRENT_PACKAGES_DIR})
 
-file(INSTALL ${SOURCE_PATH}/bin DESTINATION ${CURRENT_PACKAGES_DIR}/debug)
+if(VCPKG_TARGET_IS_WINDOWS)
+    file(INSTALL ${SOURCE_PATH}/bin DESTINATION ${CURRENT_PACKAGES_DIR}/debug)
+endif()
 file(INSTALL ${SOURCE_PATH}/lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug)
 
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${LICENSE}")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-
-else()
-# build from source
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO loot/libloot
-    REF "${VERSION}"
-    SHA512 f2496f84bce3f3f4ae502d6355bb26529d407ddf6f8780e30e63efc3e5d48693f7502f1d10a1af306f435bd6ba6d9e2b468e7c2525dae49aec075cddbba00d74
-    HEAD_REF master
-    PATCHES fix-spdlog.patch
-)
-
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        -DLIBLOOT_BUILD_TESTS=OFF
-        -DLIBLOOT_INSTALL_DOCS=OFF
-)
-
-vcpkg_cmake_install()
-
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-vcpkg_fixup_pkgconfig()
-endif()
