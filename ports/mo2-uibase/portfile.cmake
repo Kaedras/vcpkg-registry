487cfd2572b1540d99e21067c68f0970425e767f
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-01-22 13:19:15 +0100

    [mo2-uibase] bump to v2.6.0-dev.5-r1

--- ports/mo2-uibase/portfile.cmake
+++ ports/mo2-uibase/portfile.cmake
@@ -7,10 +7,10 @@ set(VCPKG_POLICY_SKIP_COPYRIGHT_CHECK enabled)
 
 if(VCPKG_TARGET_IS_WINDOWS)
     set(OS windows)
-    set(SHA 97d7fe5baed72eb3ba4cd769d1e32a1cff10f0962af7a39c01783f9c64cd45cf2a76ae2ec40372249d59c6b9a7c5ad7d27765ec9d20c12ffc44af8638bcdf463)
+    set(SHA e8757c1d67cb8aaec7df36f9c8e3bb9dcac8eae6f197becc8ef70fda253e7fbbc282c05b0bd43a4401207ce3706e3be32ae71237226b3ec2cfc928accf85ad3b)
 else()
     set(OS linux)
-    set(SHA fc8843e84a542e967556e01c9212a6c0cb21b0488a7e73d92738a9563f6d81efc8b858f1199d0b4cdf45b0ab6891285df0401dba88d4408ce28118012eede96d)
+    set(SHA e36130adc3762d04c6f5c629b88df3aebf2fddee42b23dd187d23d376264081b22d045b8781d071f886823648f9343d74c9faadf8a4db8598a4a3ab39ee00683)
 endif()
 
 vcpkg_download_distfile(ARCHIVE
