5d3b86c15ecf972ea5591d640af3e6159164e989
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-03-17 16:04:12 +0100

    [mo2-overlayfs] bump version

--- ports/mo2-overlayfs/portfile.cmake
+++ ports/mo2-overlayfs/portfile.cmake
@@ -1,14 +1,13 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Kaedras/mo2-overlayfs
-    REF cb478d87b6a7c7d1cc8a863cc08182764dad0435
-    SHA512 b346fa5e5405f0be21454dd7ec61ae1de89cee9c6a38241f8105c4835815b1cd81b1441cf7c0ecd77ba610da45d448cf5c81c0b84366357c9cd9b6cfbe1f5fb6
+    REF 07e951be41958f4fdb829b8fbc5853ea0b5cfea7
+    SHA512 46e49d958dff4e851025e7edcbe581f9c44e69ee2d5d7a6d446fbb31f56085e862ab3fcc33850f2cbee3a24ee1999807da2faca074dc93d947abdc33c55e5dbb
     HEAD_REF master
 )
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
-vcpkg_copy_pdbs()
 
 vcpkg_cmake_config_fixup(PACKAGE_NAME "mo2-overlayfs" CONFIG_PATH "lib/cmake/mo2-overlayfs")
 
