ad9123ae15ef3f916d7f88cbbc330c0d2162e3e7
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-03-23 19:10:30 +0100

    [vdf-parser] fixed a typo

--- ports/vdf-parser/vdf-parser-config.cmake
+++ ports/vdf-parser/vdf-parser-config.cmake
@@ -7,4 +7,4 @@ get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
 get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
 
 add_library(vdf-parser INTERFACE IMPORTED)
-target_include_directories(vdf-parser INTERFACE ${_IMPORT_PREFIX}/include/vdf)
+target_include_directories(vdf-parser INTERFACE ${_IMPORT_PREFIX}/include/vdf-parser)
