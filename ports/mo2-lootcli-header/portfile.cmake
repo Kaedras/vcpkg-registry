a6c0dbfa267068500d8bd6ff50fe58b2896f6b6a
Kaedras <6783810+Kaedras@users.noreply.github.com>
2025-08-05 18:58:11 +0200

    [mo2-lootcli-header] Update port to use HEADER_ONLY option

--- ports/mo2-lootcli-header/portfile.cmake
+++ ports/mo2-lootcli-header/portfile.cmake
@@ -2,11 +2,11 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Kaedras/modorganizer-lootcli
     REF ${VERSION}
-    SHA512 e563f02cff156e1ff6920cc9e8d6f65006e64fc308a0ace117893c5e5ad80f38a6ee4873755054b0377699ee76662de06184320b9ee2dcfa55ec480f01d7f87f
+    SHA512 12062f27c4ec2b4b79b141aca4a296f500887c0a3217be96f984f3445b98b3588a6d14cca9f6c17f89c6facedcdc70c6977b37cc0d54e6ba48a1dda3cd39e36e
     HEAD_REF master
 )
 
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS -DHEADER_ONLY=ON)
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(PACKAGE_NAME "mo2-lootcli-header" CONFIG_PATH "lib/cmake/mo2-lootcli-header")
