750eaa887f778997d440f2c39d8250276af4dca5
Kaedras <6783810+Kaedras@users.noreply.github.com>
2026-03-22 01:06:43 +0100

    [mo2-dxgiformat-header] Attempt to fix include location

--- ports/mo2-dxgiformat-header/portfile.cmake
+++ ports/mo2-dxgiformat-header/portfile.cmake
@@ -1,8 +1,16 @@
-vcpkg_download_distfile(ARCHIVE
+vcpkg_download_distfile(HEADER
     URLS "https://raw.githubusercontent.com/microsoft/DirectX-Headers/${VERSION}/include/directx/dxgiformat.h"
     FILENAME "dxgiformat.h"
     SHA512 004be604e28a60f8f6195144e678a82d10894fc4d1cfea10e7efa72eb7d7cd15efbd03d21ec80a65579c00eec8aa77b09809ade3f1b9198901757ebe7a63401c
 )
 
-file(INSTALL ${ARCHIVE} DESTINATION ${CURRENT_PACKAGES_DIR}/include/directx)
-file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/mo2-dxgiformat-header-config.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/mo2-dxgiformat-header)
+file(INSTALL ${HEADER} DESTINATION ${CURRENT_PACKAGES_DIR}/include/directx)
+file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/mo2-dxgiformat-header-config.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+
+# install license
+vcpkg_download_distfile(LICENSE
+    URLS "https://raw.githubusercontent.com/microsoft/DirectX-Headers/${VERSION}/LICENSE"
+    FILENAME "LICENSE"
+    SHA512 6ef2dc66f6d26b34d1302c760bfc070b3557717571ebccb0bcdac899b91e916c1b47be4cd9e8ce1b387599e99507c98b0f320b20f2997139ebccc90782c32ae7
+)
+file(INSTALL ${LICENSE} DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME "copyright")
