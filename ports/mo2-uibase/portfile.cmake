f3dd81eaaeaf548c574023321d2555cb82d2136d
Kaedras <6783810+Kaedras@users.noreply.github.com>
2026-03-30 15:18:37 +0200

    [mo2-uibase] Bump to 2.6.0-dev.24

--- ports/mo2-uibase/portfile.cmake
+++ ports/mo2-uibase/portfile.cmake
@@ -7,10 +7,10 @@ set(VCPKG_POLICY_SKIP_COPYRIGHT_CHECK enabled)
 
 if(VCPKG_TARGET_IS_WINDOWS)
     set(OS windows)
-    set(SHA 41c098d07f6bf53f0311127464935c1ad9cd271d16bdb0f9ecc2a0c6fda5f29ec299175328b4afe757edc19cbcb467a85b710c21e2bf7948bcb2eb50eb99e679)
+    set(SHA ef2b915a052b9d5191268dfa4f4e6bdbde5bd06d0622e8b249e66b8354091b95201b8a8c956b5c45d6c295ce8ef31a82cfbf7e45ee5a52b5aff2f7a3e165185d)
 else()
     set(OS linux)
-    set(SHA 8e4a4f9bf91eb144813b1ed6da5b605173c28a4131f7e55aa92fd74937148a3be75caab878584121b646d75422d87a4d1532e8e8ecf04c7729d30d1c26098486)
+    set(SHA ad555d8e644437c4e0a93695267e0e88e23b1f1dbc568afff8cb6b26cdfea81d5408947730b37241136d75409a46a454fe8817be142e70b82318ec58d7a26a56)
 endif()
 
 vcpkg_download_distfile(ARCHIVE
@@ -54,7 +54,6 @@ if(VCPKG_TARGET_IS_WINDOWS)
 else()
     file(INSTALL
         ${SOURCE_PATH}/lib/libuibase.so
-        ${SOURCE_PATH}/debug/libuibase.so.dbg
     DESTINATION
         ${CURRENT_PACKAGES_DIR}/lib
     )
@@ -66,7 +65,6 @@ else()
 
     file(INSTALL
         ${SOURCE_PATH}/lib/libuibased.so
-        ${SOURCE_PATH}/debug/libuibased.so.dbg
     DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
     file(INSTALL
         ${SOURCE_PATH}/lib/cmake
